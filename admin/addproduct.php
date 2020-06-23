e6e59dcf14543d9bc9ab8a8748f147bd119f044b
unknown <you@example.com>
2020-06-23 12:46:27 +0700

    Làm xong giao diện và thêm các file xử lý php

---
+++ admin/addproduct.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <script src="../js/bootstrap.min.js"></script>
+    <title>Thêm sản phẩm</title>
+    <?php
+        $a=1;
+    ?>
+</head>
+<body>
+    <div class="container">
+    <h2>Thêm sản phẩm</h2>
+    <form action="xlyaddProduct.php" metho="POST">
+        <div class="form-group">
+            <label for="email">Tên sản phẩm:</label>
+            <input type="text" class="form-control" id="name" placeholder="Tên sản phẩm" name="nameProduct">
+        </div>
+        <div class="form-group">
+            <label for="pwd">Ảnh sản phẩm:</label>
+            <input type="file" id="" name="image">
+        </div>
+        <div class="form-group">
+            <label for="email">Giá sản phẩm:</label>
+            <input type="number" class="form-control" id="price" placeholder="Giá sản phẩm" name="price">
+        </div>
+        <div class="form-group">
+            <label for="description">Mô tả sản phẩm</label>
+            <textarea class="form-control" id="description" rows="3" name="description" placeholder="Mô tả sản phẩm"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+    </div>
+</body>
+</html>
---
+++ admin/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="https://kit.fontawesome.com/fe9872fc12.js" crossorigin="anonymous"></script>
+    <title>Trang quản trị</title>
+</head>
+<body>
+    <h1 class="text-center">Chào mừng bạn đến với trang quản trị</h1>
+    <div class="d-flex justify-content-center mt-5">
+        <button class="btn btn-info mr-5"><a class="text-white font-weight-bold" style="text-decoration:none" href="listproduct.php">Xem danh sách sản phẩm</a></button>
+        <button class="btn btn-danger"><a class="text-white font-weight-bold" style="text-decoration:none" href="addproduct.php">Thêm sản phẩm mới</a></button>
+    </div>
+    
+</body>
+</html>
