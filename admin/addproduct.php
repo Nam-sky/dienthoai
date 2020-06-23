7d045382e0aef473bfe879361ac5b96205e6e02b
Nam-sky <nguyenvannam13101997@gmail.com>
2020-06-23 13:50:17 +0700

    add thêm code phần addPtoduct

--- admin/addproduct.php
+++ admin/addproduct.php
@@ -14,14 +14,14 @@
 <body>
     <div class="container">
     <h2>Thêm sản phẩm</h2>
-    <form action="xlyaddProduct.php" metho="POST">
+    <form action="xlyaddProduct.php" method="POST" enctype="multipart/form-data"> 
         <div class="form-group">
             <label for="email">Tên sản phẩm:</label>
             <input type="text" class="form-control" id="name" placeholder="Tên sản phẩm" name="nameProduct">
         </div>
         <div class="form-group">
             <label for="pwd">Ảnh sản phẩm:</label>
-            <input type="file" id="" name="image">
+            <input type="file" id="" name="fileToUpload" id="fileToUpload">
         </div>
         <div class="form-group">
             <label for="email">Giá sản phẩm:</label>
